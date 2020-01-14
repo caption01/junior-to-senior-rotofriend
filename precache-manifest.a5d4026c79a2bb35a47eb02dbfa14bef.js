57da1f758dd8ac7bb33d9b37424460c049c3e621
nutchapon <nutchapon.han@gmail.com>
2020-01-14 09:20:56 +0700

    Updates

---
+++ precache-manifest.a5d4026c79a2bb35a47eb02dbfa14bef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ada860d166d5626dc149b7e5015a172",
+    "url": "/caption01/junior-to-senior-rotofriend/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/caption01/junior-to-senior-rotofriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "43544e3771dd0e8ce5ef",
+    "url": "/caption01/junior-to-senior-rotofriend/static/css/main.040cb408.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/caption01/junior-to-senior-rotofriend/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "43544e3771dd0e8ce5ef",
+    "url": "/caption01/junior-to-senior-rotofriend/static/js/main.5d3e1d3f.chunk.js"
+  },
+  {
+    "revision": "3a6d7ec1ea6e09ff80f3",
+    "url": "/caption01/junior-to-senior-rotofriend/static/js/runtime-main.22b52ec8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/caption01/junior-to-senior-rotofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
